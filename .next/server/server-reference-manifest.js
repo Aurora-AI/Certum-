7c429a6dcc60b191e758a481cdc33e822ebc3a69
Rodrigo Cesar Winhaski <rodrigo@auroracrm.com.br>
2026-02-02 14:53:57 -0300

    chore(security): Upgrade Next.js to 16.1.6 to fix CVE-2025-66478

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ODI+bDsn36mJA1aRSGITKM1yDzMEuRgBSnEua/uqQfc=\"\n}"
